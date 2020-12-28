6926478fc9e179f079240e720889db1f841860b3
Diego Romero <dr87152@gmail.com>
2020-12-27 20:46:58 -0600

    path

--- src/index.js
+++ src/index.js
@@ -2,23 +2,20 @@ const express = require('express');
 const { dirname } = require('path');
 console.log("diego")
 const app= express();
-const path =require('path')
+const DRM =require('path')
 
 
 
 app.get('/',(req,res) =>{
-    res.sendFile(path.join(__dirname, '/HTML/index.html'));
+    res.sendFile(DRM .join(__dirname, '/HTML/index.html'));
 })
 
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(DRM .join(__dirname,'public')))
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
+    console.log(`DRM ${ PORT }`);
 });
 
 
-app.listen(app.get('port'), ()=>{
-    
-})
