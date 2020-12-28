46daecb76301f03373b9867505d619a70444b8b1
Diego Romero <dr87152@gmail.com>
2020-12-27 20:36:59 -0600

    landingpage

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const { dirname } = require('path');
 console.log("diego")
 const app= express();
 const path =require('path')
-app.set('port',4000);
+
 
 
 app.get('/',(req,res) =>{
@@ -12,6 +12,12 @@ app.get('/',(req,res) =>{
 
 
 app.use(express.static(path.join(__dirname,'public')))
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
+
 
 app.listen(app.get('port'), ()=>{
     
