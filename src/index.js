7c609640d03076e824a5ea1717d80f6613bf5644
Diego Romero <dr87152@gmail.com>
2021-01-07 08:56:53 -0600

    database

--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@ const { dirname } = require('path');
 console.log("diego")
 const app= express();
 const DRM =require('path')
+const {Pool} =require('pg')
+
+/*
+
+ const configDataBase = {
+    user:'cyeeqhvzfmwfjo',
+    host:'ec2-54-157-12-250.compute-1.amazonaws.com',
+    password:'',
+    database:'dct9ja4j2fhrti'
 
 
+}
+
+
+
+const db= new Pool(configDataBase)
+
+
+*/
 
 app.get('/',(req,res) =>{
     res.sendFile(DRM .join(__dirname, '/HTML/index.html'));
